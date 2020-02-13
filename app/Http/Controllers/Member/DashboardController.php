42835b43f012d33f69c64c86f975a6d76093720e
HusniMu <husni060300@gmail.com>
2020-02-13 20:33:11 +0700

    update member dashboard

--- app/Http/Controllers/Member/DashboardController.php
+++ app/Http/Controllers/Member/DashboardController.php
@@ -11,15 +11,8 @@ class DashboardController extends Controller
     public function index()
     {
         $user = Auth::user();
-        $posts = $user->posts;
-        $popular_posts = $user->posts()
-                    ->withCount('comments')
-                    ->withCount('favorite_to_users')
-                    ->orderBy('view_count','desc')
-                    ->orderBy('comments_count','desc')
-                    ->orderBy('favorite_to_users_count','desc')
-                    ->take(5)->get();
-        $all_views = $posts->sum('view_count');
-        return view('member.dashboard');
+        $comments = $user->comments;
+        $favorites = $user->favorite_posts;
+        return view('member.dashboard',compact('comments','favorites'));
     }
 }
--- resources/views/member/dashboard.blade.php
+++ resources/views/member/dashboard.blade.php
@@ -23,8 +23,58 @@
                 <div class="single-post info-area ">
 
                     <div class="about-area">
-                        <h4 class="title"><b>MEMBER PROFILE</b></h4>
-                        <h5>test</h5>
+                        <h4 class="title"><b>MEMBER COMMENTS HISTORY</b></h4>
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th>
+                                        #
+                                    </th>
+                                    <th>
+                                        comment
+                                    </th>
+                                    <th>
+                                        post
+                                    </th>
+                                    <th>
+                                        at
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($comments as $key=>$comment)
+                                    <tr>
+                                        <td>{{$key+1}}</td>
+                                        <td>{{$comment->comment}}</td>
+                                        <td>{{$comment->post_id}}</td>
+                                        <td>{{$comment->created_at->diffForHumans()}}</td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                        <h4 class="title"><b>MEMBER FAVORITE HISTORY</b></h4>
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>liked post</th>
+                                    <th>at</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($favorites as $key=>$favorite)
+                                    <tr>
+                                        <td>{{$key+1}}</td>
+                                        <td>
+                                            <a href="{{route('post.details',$favorite->slug)}}">
+                                                {{$favorite->title}}
+                                            </a>
+                                        </td>
+                                        <td>{{$favorite->created_at->diffForHumans()}}</td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
                     </div>
 
 
